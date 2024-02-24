53cda26bc60da55e84a0fc72216449d77af51cdc
NaoCraftLab <toxic.swoops_0j@icloud.com>
2024-02-24 22:41:08 +0400

    #TFCPR-7: Configure build and release

--- build.sh
+++ build.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env.sh bash
-export RESOURCE_PACK_NAME=TFC-Patchouli-RUS
-if [ -z "$RESOURCE_PACK_VERSION" ]; then
-    export RESOURCE_PACK_VERSION=DEV
+
+# Проверка на наличие аргумента (тега)
+if [ -z "$1" ]; then
+  echo "Тег не указан!"
+  exit 1
 fi
-export ARTEFACT_NAME=$RESOURCE_PACK_NAME-$RESOURCE_PACK_VERSION.zip
+TAG=$1
+REPO_NAME=$(basename -s .git `git config --get remote.origin.url`)
 
-export WORKING_DIR="$PWD"
-export SOURCE_BOOK_DIR=$WORKING_DIR/src/main
-export SOURCE_PACK_METADATA_DIR=$WORKING_DIR/src/resources
-export BUILD_DIR=$WORKING_DIR/build
-export TARGET_BOOK_DIR=$BUILD_DIR/raw/assets/tfc/patchouli_books/book/ru_ru
-export TARGET_PACK_METADATA_DIR=$BUILD_DIR/raw
+WORKING_DIR="$PWD"
+BUILD_DIR="${WORKING_DIR}/build"
+ARTIFACT_NAME="${REPO_NAME}-${TAG}.zip"
 
-echo "Remove build directory"
-rm -rf $BUILD_DIR
+# Определение папки исходников на основе тега
+if [[ $TAG == 1.*.* ]]; then
+  cd "${WORKING_DIR}/src/tfc"
+elif [[ $TAG == 2.*.* ]]; then
+  cd "${WORKING_DIR}/src/tfc2"
+elif [[ $TAG == 3.*.* ]]; then
+  cd "${WORKING_DIR}/src/tfc3"
+else
+  echo "Неверный формат тега! Должен быть 1.x.x, 2.x.x или 3.x.x"
+  exit 1
+fi
 
-echo "Create build dir"
-mkdir -p $BUILD_DIR
+# Создание директории для сборки, если она не существует
+mkdir -p ${BUILD_DIR}
 
-echo "Copy sources"
+# Сборка артефакта
+zip -q -0 -r "${BUILD_DIR}/${ARTIFACT_NAME}" * >/dev/null 2>&1
 
-echo TARGET_BOOK_DIR=$TARGET_BOOK_DIR
-mkdir -p $TARGET_BOOK_DIR
-cp -rp $SOURCE_BOOK_DIR/* $TARGET_BOOK_DIR/
+echo "Артефакт собран: ${BUILD_DIR}/${ARTIFACT_NAME}"
 
-echo TARGET_PACK_METADATA_DIR=$TARGET_PACK_METADATA_DIR
-mkdir -p $TARGET_PACK_METADATA_DIR
-cp -rp $SOURCE_PACK_METADATA_DIR/* $TARGET_PACK_METADATA_DIR/
-
-echo "Build"
-cd $BUILD_DIR/raw
-echo ARTEFACT_NAME=$ARTEFACT_NAME
-zip -q -0 -r $BUILD_DIR/$ARTEFACT_NAME * >/dev/null 2>&1
-cd $WORKING_DIR
+cd ${WORKING_DIR}
