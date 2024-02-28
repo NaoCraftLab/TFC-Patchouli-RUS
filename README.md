cb4132f76bd96298006574a13cd8e985dde98ce6
NaoCraftLab <toxic.swoops_0j@icloud.com>
2024-02-29 02:12:19 +0400

    Use TFC fork only

--- README.md
+++ README.md
@@ -2,38 +2,14 @@
 
 Ресурспак с переводом руководства TerraFirmaCraft от [NaoCraftLab](https://github.com/NaoCraftLab).
 
-
-
 ## Сборка
 
-### TerraFirmaCraft 2
+### Пререквизиты
 
-![tfc2-categories.png](publication%2Fgalery%2Ftfc2-categories.png)
-
-Сборка ресурспака для TFC2 основана на ресурсах данного репозитория.
-
-#### Команда сборки для MacOS/Linux
-
-```bash
-./build.sh "1.12.2" "1"
-```
-
-где "1.12.2" - версия Minecraft, а "1" - номер сборки ресурспака.
-
-Результат в виде zip-архива будет находиться в папке [build](./build).
-
-### TerraFirmaCraft 3
-
-![tfc3-categories.png](publication%2Fgalery%2Ftfc3-categories.png)
-
-Сборка ресурспака для TFC3 основана на ресурсах [официального перевода от NaoCraftLab](https://github.com/NaoCraftLab/TerraFirmaCraft).
-
-#### Пререквизиты
-
-- Python 3.10
+- Python 3.10 (для версий 1.18+)
 - Клон репозитория [официального перевода от NaoCraftLab](https://github.com/NaoCraftLab/TerraFirmaCraft)
 
-#### Команда сборки для MacOS/Linux
+### Команда сборки для MacOS/Linux
 
 ```bash
 ./build.sh "1.20.1" "1" "/nao_craft_lab_tfc_path"
--- build.sh
+++ build.sh
@@ -16,28 +16,36 @@ if [ -z "$2" ]; then
 fi
 BUILD_NUMBER=$2
 
-if [ "$MC_MINOR_VERSION" -gt "17" ]; then
-    if [ -z "$3" ]; then
-      echo "Необходимо указать директорию проекта TFC!"
-      exit 1
-    fi
-    TFC_DIR=$3
+if [ -z "$3" ]; then
+  echo "Необходимо указать директорию проекта TFC!"
+  exit 1
 fi
+TFC_DIR=$3
 
 BUILD_DIR="${WORKING_DIR}/build"
 RAW_DIR=${BUILD_DIR}/raw
 PATCHOULI_SOURCE_DIR="${TFC_DIR}/resources"
-PATCHOULI_BUILD_DIR="${TFC_DIR}/src/main/resources/assets/tfc/patchouli_books/field_guide/ru_ru"
 PATCHOULI_TARGET_DIR=${RAW_DIR}/assets/tfc/patchouli_books/field_guide/ru_ru
+
+if [ "$MC_MINOR_VERSION" -gt "19" ]; then
+    PATCHOULI_BUILD_DIR="${TFC_DIR}/src/main/resources/assets/tfc/patchouli_books/field_guide/ru_ru"
+elif [ "$MC_MINOR_VERSION" -eq "18" ]; then
+    PATCHOULI_BUILD_DIR="${TFC_DIR}/src/main/resources/data/tfc/patchouli_books/field_guide/ru_ru"
+elif [ "$MC_MINOR_VERSION" -eq "12" ]; then
+    PATCHOULI_BUILD_DIR="${TFC_DIR}/src/main/resources/assets/tfc/patchouli_books/book/ru_ru"
+else
+    echo "Сборка для Minecraft ${MC_VERSION} не поддерживается!"
+    exit 1
+fi
 
 PROJECT_NAME=$(basename -s .git `git config --get remote.origin.url`)
 ARTIFACT_NAME="${PROJECT_NAME}-${MC_VERSION}-${BUILD_NUMBER}.zip"
 
 echo "Сборка: ${ARTIFACT_NAME}"
+echo "Перевод будет взят из директории ${PATCHOULI_BUILD_DIR}"
 
 echo "Очистка директории ${BUILD_DIR}"
 rm -rf $BUILD_DIR
-#mkdir -p $BUILD_DIR
 mkdir -p $RAW_DIR
 
 if [ "$MC_MINOR_VERSION" -gt "17" ]; then
@@ -50,11 +58,11 @@ if [ "$MC_MINOR_VERSION" -gt "17" ]; then
     cd ..
     python3.10 ./resources/__main__.py --translate ru_ru book
     cd $WORKING_DIR
-
-    echo "Копирование файлов перевода"
-    mkdir -p $PATCHOULI_TARGET_DIR
-    cp -rp ${PATCHOULI_BUILD_DIR}/* $PATCHOULI_TARGET_DIR
 fi
+
+echo "Копирование файлов перевода"
+mkdir -p $PATCHOULI_TARGET_DIR
+cp -rp ${PATCHOULI_BUILD_DIR}/* $PATCHOULI_TARGET_DIR
 
 echo "Копирование метаданных ресурспака"
 cp -rp ./src/${MC_SHORT_VERSION}/* $RAW_DIR
